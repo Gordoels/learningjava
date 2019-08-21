8c27493979a85e14e6b586e8a6bdb48b21a9d09c
Gordoels <lordservio@gmail.com>
2019-08-21 00:49:26 -0300

    Demonstração de recuperação de dados no jdbc

---
+++ Learning16jdbc2/src/app/Pograminha.java
@@ -0,0 +1,34 @@
+package app;
+import db.DB;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Pograminha { //SET GLOBAL time_zone = '+3:00'; <--- na linha de cmd mysql caso o dê erro de timezone
+
+	public static void main(String[] args) {
+		
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		try {
+			conn = DB.getConn();
+	
+			st = conn.createStatement(); //objeto statement instanciado
+			
+			rs = st.executeQuery("select * from department");
+			
+			while (rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name")); //comando sql consultando a tabela, trazendo o resultado para a variavel rs
+			}
+		}catch (SQLException e) {
+			e.printStackTrace();
+		}
+		DB.closeConnection();
+		DB.closeStat(st);
+		DB.closeResSet(rs);
+
+	}
+
+}
